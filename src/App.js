caa10550a69a7c5c2c3be8c71483262c2a64b377
rohini251103610 <rohinirachelr@gmail.com>
2024-08-27 17:33:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <td class="w" align="center" >♜</td>
+          <td class="b" align="center">♞</td>
+          <td class="w" align="center">♝</td>
+          <td class="b" align="center">♛</td>
+          <td class="w" align="center">♚</td>
+          <td class="b" align="center">♝</td>
+          <td class="w" align="center">♞</td>
+          <td class="b" align="center">♜</td>
+
+        </tr>
+        <tr>
+           
+           <td class="b b-p" align="center"> ♟</td>
+           <td class="w b-p"  align="center"> ♟</td>
+           <td class="b b-p"  align="center"> ♟</td>
+           <td class="w b-p"  align="center"> ♟</td>
+           <td class="b b-p"  align="center"> ♟</td>
+           <td class="w b-p"  align="center"> ♟</td>
+           <td class="b b-p"  align="center"> ♟</td>
+           <td class="w b-p"  align="center"> ♟</td>
+       </tr>
+        <tr>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>  
+        </tr>
+        <tr>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            
+        </tr>
+        <tr>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+        </tr>
+        <tr>
+           
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+            <td class="b"></td>
+            <td class="w"></td>
+        </tr>
+        <tr>
+            <td class="w w-p"  align="center">♙</td>
+            <td class="b w-p"  align="center">♙</td>
+            <td class="w w-p"  align="center">♙</td>
+            <td class="b w-p"  align="center">♙</td>
+            <td class="w w-p"  align="center">♙</td>
+            <td class="b w-p"  align="center">♙</td>
+            <td class="w w-p"  align="center">♙</td>
+            <td class="b w-p"  align="center">♙</td>
+        </tr>
+        <tr>
+            
+            <td class="b"  align="center">♖</td>
+            <td class="w"  align="center">♘</td>
+            <td class="b"  align="center">♗</td>
+            <td class="w"  align="center">♕</td>
+            <td class="b"  align="center">♔</td>
+            <td class="w"  align="center">♗</td>
+            <td class="b"  align="center">♘</td>
+            <td class="w"  align="center">♖</td>
+        </tr>
+       
+    </table>
     </div>
   );
 }
 
 export default App;
+
+
